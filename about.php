34d49732b19ce2d37cc022d2eef38836d7c19711
Juan Alvarado <jalvarado91@gmail.com>
2016-09-14 00:39:10 -0400

    needed pages

---
+++ about.php
@@ -0,0 +1,157 @@
+<?php
+$bodyClass = "about";
+include('./includes/head.php'); ?>
+
+<div class="content">
+  <div class="container">
+    <h1>About Us</h1>
+  </div>
+  <div class="narrow-container">
+    <h2 class="heading heading-underline">The Directors</h2>
+    <p class="lead center mb-lg">
+      Julie and Matt Smith bring to your children 30 plus years of their own
+      professional experiences, hard work and dedication in their performing
+      arts. With this advanced experience, students will have the
+      opportunity to work in a professional atmosphere and be directed by
+      true professionals.
+    </p>
+  </div>
+  <br>
+  <div class="container">
+
+    <div class="staff director clearfix">
+      <div class="image">
+        <img class="img-responsive" src="/assets/img/julie_smith.jpg" alt="" />
+      </div>
+      <div class="info">
+        <h4 class="name">Julie Pappas Smith</h4>
+        <p class="title">Director</p>
+        <div class="bio">
+          <p>
+            Julie Pappas Smithreceived her BFA in musical theatre from the
+            Boston Conservatory of Music. her professional career has taken
+            her around the world. She has had the privilege of working with
+            great artists such as Harold Prince, Susan Stroman, Baayork
+            Lee, Ben Vereen, Chloris Leachman and Len Cariou to name a few.
+            Julie has toured with Broadway National Tours as well as
+            European Tours, lived and performed in Tokyo and sang at the
+            Royal Albert Hall in London. Ms.Smith sang for Norwegian and
+            Carnival Cruise Lines and has dabbled in a few Films, Television
+            and Commercial spots.
+          </p>
+          <p>
+            Now as the "Teacher" her role has changed from the performer to
+            the dedicated instructor and director. Julie has directed and
+            choreographed for numerous musicals in magnet schools, theatre
+            arts schools and childrens theatre in the Miami area and in New
+            Hampshire. Julie Smith currently works at Ransom Everglades
+            middle school as the Dance director and has worked at NWSA, UM,
+            FIU, Gulliver, Belen, Southwood Middle School, ABC and Jill
+            Mallory Dance Studio. She is also on summer staff for Miami City
+            Ballet, Marblehead School of Ballet and The Boston Conservatory
+            of Music's Summer Musical Theatre Dance Intensive.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="staff director clearfix">
+      <div class="image">
+        <img class="img-responsive" src="/assets/img/matt_smith.jpg" alt="" />
+      </div>
+      <div class="info">
+        <h4 class="name">Matt Smith</h4>
+        <p class="title">Director</p>
+        <div class="bio">
+          <p>
+            Matt Smith has been playing drums for 38 years and holds a degree from Berklee College of Music. His professional career includes playing worldwide with such artists as Charro, John Davidson, members of the Buddy Rich Big Band and the Stan Kenton Orchestra as a show band drummer or Carnival Cruise Lines, playing at Disney in The Hollywood Hitmen, and original touring bands in the styles of ska/reggae, rock, and gospel, and also as an educator and recreational group drumming facilitator.
+          </p>
+          <p>
+            He has recently played drums for dance classes and workshops and teaches classes he designed called Rhythm RebelsTM where he teaches world percussion rhythms and Step in Time where he teaches music theory and rhythm for dancers so they understand the special relationship between dance and music. He is also a guest artist in local schools where he teaches leadership and teamwork skills through the use of drumming. Most recently he has become a certified meditation instructor and uses the power of drums to help people in their pursuit of wellness.
+          </p>
+          <p>
+            He brings his extensive knowledge and experience as a musician together with his wife Julie’s expertise in musical theater with the goal of teaching future professionals the skills necessary to make it in the professional world. He is also equally passionate about making music easily accessible to anyone who just wants to experience the joys of making music without the desire to take it to the professional level.
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+    <h2 class="heading heading-underline">The Faculty</h2>
+
+    <div class="staff director clearfix">
+      <div class="image">
+        <img class="img-responsive" src="/assets/img/priscilla_blanco.jpeg" alt="" />
+      </div>
+      <div class="info">
+        <h4 class="name">Priscilla Blanco</h4>
+        <p class="title">Piano,Music Theory, Voice, Pre-Pro</p>
+        <div class="bio">
+          <p>
+            Priscilla attended Florida International
+            University for her undergraduate and graduate studies in music education. Priscilla
+            studied piano with Dr. Nancy Luzko and Dr. Jose Lopez and studied voice with
+            Dr. Kathleen Wilson while at FIU. She taught in the public school system for five years
+            while holding leadership positions as Secretary and then President with the Dade
+            County Music Educators Association. Priscilla has been the recipient of the Rookie
+            Teacher of the Year for the central region and has collaborated in organizing district
+            wide festivals for MDCPS students.
+          </p>
+          <p>
+            Priscilla is also the co-founder of Spark! Music Initiative, a non-profit based in
+            Grand Cayman, which focuses on music making in a creative light. In addition,she music directs locally having worked on shows like Gypsy, Songs for a New World, Legally Blonde, Violet, Edges, Rent, Damn Yankees, Wedding Singer, Crazy for You, Willy Wonka, Cinderella, 42ndStreet,  among others. Priscilla also works as a vocal coach and accompanist throughout South Florida.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="staff director clearfix">
+      <div class="image">
+        <img class="img-responsive" src="/assets/img/jessica_fox.jpeg" alt="" />
+      </div>
+      <div class="info">
+        <h4 class="name">Jessica Fox</h4>
+        <p class="title">Acting Workshops/ Private</p>
+        <div class="bio">
+          <p>
+            Jessica Fox has a Bachelor of Arts degree in English and Theatre and a
+            Master of Arts Degree in Directing from the Chicago College of the Performing Arts.
+            She has over twelve years of educational theatre, performing, and directing
+            experience. She teaches drama classes at a Miami Private school, is a member of the
+            Educational Theatre Association and sponsors two Thespian troupes. Her students
+            consistently earn superior scores on their  individual scenes and one act
+            performances. Some of Jessica’s directing credits include Almost Maine, Arsenic and Old Lace, Beauty
+            and the Beast, Columbinus, Harvey, The Importance of Being Earnest, The King and I, The Laramie Project, A Midsummer Night’s Dream, The Miracle Worker, Our Hearts Were Young and Gay, Peter Pan, Something’s Afoot, Stefanie Hero, Thoroughly Modern Millie, The Wizard of Oz (RSC), and You’re a Good Man, Charlie Brown. Jessica enjoys working with passionate students who are committed to developing their gifts and talents.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="staff director clearfix">
+      <div class="image">
+        <img class="img-responsive" src="/assets/img/alejandro_senior.jpeg" alt="" />
+      </div>
+      <div class="info">
+        <h4 class="name">Alejandro Senior</h4>
+        <p class="title">Piano, Voice, Intro to Musical Theatre, Pre-Pro</p>
+        <div class="bio">
+          <p>
+            Alejandro Senior was born in Santo Domingo, Dominican Republic and moved to Miami in 1999. After graduating from Belen Jesuit Preparatory School, he was accepted into the University of Miami’s Frost School of Music and began his Freshman fall semester in 2013. He is studying Music Education and Music Business as a Classical Piano Principle. He studied under Leonardo Curbelo for Classical Piano and under Ana Flavia Zuim. He’s Music Directed over 25 productions in the last two years. Some favorite credits as Music Director include: Beauty and The Beast (Belen), Addams Family (Belen), Little Shop of Horrors (Belen), CATS (Actors Playhouse), Lion King (Scheck Hillel) Frog and Toad (Area Stage Company. Recently, ‘Made of Sugar’, a film Alejandro composed an original score for, received recognition by the Academy Awards, and was nominated for a Student OSCAR Award. Alejandro was also contracted by Jean’s Playhouse for IMPACT’s Summer 2016 Session. He wrote over 52 songs for original Children’s Theater productions. Alejandro plans to finish his undergrad and become a musical director for a professional theater company. For more information: www.alejandrosenior.com .
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="staff director clearfix">
+      <div class="info">
+        <h4 class="name">Jesse Carlo</h4>
+        <p class="title">Special Guest Teacher</p>
+        <div class="bio">
+          <p>
+            Jesse Carlo is a seasoned performer and scholar. His professional performance career includes, B’way: The Capeman, Footloose; B’way National Tours: Gypsy, Evita, My Fair Lady, Smokey Joes Café, as well as a host of Regional Theatre productions. Jesse is also an international director, choreographer, and arts educator. He currently teaches Humanities and Philosophy at Miami Dade College. He holds an M.S. Ed. in Interdisciplinary Arts and is completing his dissertation for a Ph.D. in Interdisciplinary Studies with a concentration in Humanities and Culture/Social Justice, Social Change.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<?php include('./includes/footer.php'); ?>
---
+++ dance.php
@@ -0,0 +1,204 @@
+<?php
+$bodyClass = "programs dance";
+include('./includes/head.php'); ?>
+
+  <div class="content">
+    <div class="container">
+      <h1>Dance Programs</h1>
+
+      <div class="all-programs">
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+        <div class="program">
+          <h2 class="title">Ballet For Broadway I</h2>
+          <p class="schedule">
+            Wednesday | 4:30pm to 6:00pm
+          </p>
+          <p class="attire">
+            <strong>Attire</strong>: Hair in a neat bun and tight, leotards, tights, ballet skirts ok, ballet slippers
+          </p>
+          <div class="description">
+            <p>
+              Basic Ballet fundamentals for the beginner dancer. Broadway
+              dancers still need a classical foundation therefore the  class
+              begins with the ballet barre, learning vocabulary and working on
+              correcting body alignment. We then take the barre into the center
+              of the room and across the floor.  We end the class by working on
+              a small combination developing the ability to connect the steps
+              using clear transitions and  challenging your memory. This is also
+              a time to begin to work on performance skills. Students progress
+              to the next level when they have developed a good understanding
+              for basic ballet technique.
+            </p>
+          </div>
+          <div class="cta">
+            <a href="#" class="button">Register</a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+<?php include('./includes/footer.php'); ?>
